0d216e1228bcbe44f656d501f744ce149f213662
Guillermo Fanjul <fafnir0007@gmail.com>
2023-03-18 15:01:04 -0400

    update: env keys

--- src/utils/api/axiosConfiguration.ts
+++ src/utils/api/axiosConfiguration.ts
@@ -3,9 +3,9 @@ const directusInstance = axios.create({
     baseURL: '/directus',
     headers:{
         'Content-Type': 'application/json',
-        'CF-Access-Client-Id': process.env.NEXT_PUBLIC_CF_ACCESS_CLIENT_ID,
-        'CF-Access-Client-Secret': process.env.NEXT_PUBLIC_CF_ACCESS_CLIENT_SECRET,
-        'Authorization': process.env.NEXT_PUBLIC_AUTH_TOKEN_DIRECTUS
+        'CF-Access-Client-Id': process.env.CF_ACCESS_CLIENT_ID,
+        'CF-Access-Client-Secret': process.env.CF_ACCESS_CLIENT_SECRET,
+        'Authorization': process.env.AUTH_TOKEN_DIRECTUS
     }
 });
 
@@ -13,9 +13,9 @@ const miliSearchInstance = axios.create({
     baseURL: '/meilisearch',
     headers:{
         'Content-Type': 'application/json',
-        'CF-Access-Client-Id': process.env.NEXT_PUBLIC_CF_ACCESS_CLIENT_ID,
-        'CF-Access-Client-Secret': process.env.NEXT_PUBLIC_CF_ACCESS_CLIENT_SECRET,
-        'Authorization': process.env.NEXT_PUBLIC_AUTH_TOKEN_MILISEARCH
+        'CF-Access-Client-Id': process.env.CF_ACCESS_CLIENT_ID,
+        'CF-Access-Client-Secret': process.env.CF_ACCESS_CLIENT_SECRET,
+        'Authorization': process.env.AUTH_TOKEN_MILISEARCH
     }
 });
 
